001aed2007f11019f122dba4afbaf3388c8bbc7a
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-10-12 13:51:08 +0545

    Add objs.py

---
+++ src/objs.py
@@ -0,0 +1,17 @@
+import json
+from os import path
+
+import telebot
+from models import dbQuery
+from tikTokScraper import getVideo
+
+#! Finding the absolute path of the config file
+scriptPath = path.abspath(__file__)
+dirPath = path.dirname(scriptPath)
+configPath = path.join(dirPath,'config.json')
+
+config = json.load(open(configPath))
+bot = telebot.TeleBot(config['botToken'], parse_mode='HTML')
+language = json.load(open(config['language']))
+
+dbSql = dbQuery(config['database'], config['videoDatabase'])
