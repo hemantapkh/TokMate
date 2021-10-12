e17d3ed86980fbceb260cad16076aefe55f0dcf2
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-10-12 13:54:10 +0545

    Add keyboard.py

---
+++ src/keyboard.py
@@ -0,0 +1,30 @@
+from src.objs import *
+
+#: Buttons for results
+def resultButton(userLanguage, link, inline=False):
+    markup = telebot.types.InlineKeyboardMarkup()
+
+    markup.add(telebot.types.InlineKeyboardButton(text=language['joinChannelBtn'][userLanguage], url="https://t.me/h9youtube"), telebot.types.InlineKeyboardButton(text=language['joinDiscussionBtn'][userLanguage], url="https://t.me/h9discussion"))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['linkBtn'][userLanguage], url=link))
+    
+    if not inline:
+        markup.add(telebot.types.InlineKeyboardButton(text=language['inlineQueryBtn'][userLanguage], switch_inline_query=''))
+
+    return markup
+
+#: Buttons for social links
+def socialKeyboard(userLanguage):
+    markup = telebot.types.InlineKeyboardMarkup()
+
+    markup.add(telebot.types.InlineKeyboardButton(text=language['joinChannelBtn'][userLanguage], url="https://t.me/h9youtube"), telebot.types.InlineKeyboardButton(text=language['joinDiscussionBtn'][userLanguage], url="https://t.me/h9discussion"))
+
+    return markup
+
+#: Button for start command
+def startKeyboard(userLanguage):
+    markup = telebot.types.InlineKeyboardMarkup()
+
+    markup.add(telebot.types.InlineKeyboardButton(text=language['inlineQueryBtn'][userLanguage], switch_inline_query_current_chat=''))
+    markup.add(telebot.types.InlineKeyboardButton(text=language['inlineQuery2Btn'][userLanguage], switch_inline_query=''))
+
+    return markup
