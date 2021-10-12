a4cdf2ac762a81d06322504c8454adbf762ac702
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-10-12 14:04:53 +0545

    Add migrations.py

---
+++ migrations.py
@@ -0,0 +1,66 @@
+import sqlite3
+import os, json
+
+config = json.load(open('src/config.json'))
+videoDatabase = config['videoDatabase']
+database = config['database']
+
+input1 = input('Do you want to run migration for video database? Y/[n]: ')
+if input1 == 'y':
+    if os.path.exists(videoDatabase):
+        os.remove(videoDatabase)
+        print('[-] Database already exists. Deleting it.')
+
+    conn = sqlite3.connect(videoDatabase)
+    print('[+] Magnetlink Database opened successfully.')
+
+    conn.execute('''CREATE TABLE URL
+            (url   TEXT PRIMARY KEY,
+            rc     TEXT NOT NULL
+            );''')
+
+    conn.execute('''CREATE TABLE RC
+            (rc       TEXT PRIMARY KEY,
+            description  TEXT,
+            duration     TEXT,
+            videoId      TEXT
+            );''')
+
+    print('[+] Table url created successfully.')
+    print('[+] Table rc created successfully.')
+
+    conn.close()
+
+input1 = input('Do you want to run migration for bot database? Y/[n]: ')
+if input1 == 'y':
+    if os.path.exists(database):
+        os.remove(database)
+        print('[-] Database already exists. Deleting it.')
+
+    conn = sqlite3.connect(database)
+    print('[+] Database opened successfully.')
+
+    conn.execute('''CREATE TABLE users
+            (UserId       INTEGER PRIMARY KEY,
+            date          STRING  NOT NULL
+            );''')
+
+    print('[+] Table users created successfully.')
+
+    conn.execute('''CREATE TABLE settings
+            (ownerId       INTEGER PRIMARY KEY,
+            language       TEXT DEFAULT "english"
+            );''')
+
+    print('[+] Table settings created successfully.')
+
+    conn.execute('''CREATE TABLE flood
+         (ownerId       INTEGER PRIMARY KEY,
+         warned         INTEGER DEFAULT 0,
+         lastMessage   INTEGER DEFAULT 0,
+         blockTill     INTEGER DEFAULT 0
+         );''')
+         
+    print('[+] Table flood created successfully.')
+
+    conn.close()
