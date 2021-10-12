d6f56befa9b1a6df89b53b0585d80c784bd4397b
Hemanta Pokharel <hemantapkh@yahoo.com>
2021-10-12 14:17:01 +0545

    Add message.py

---
+++ src/message.py
@@ -0,0 +1,58 @@
+from src.objs import *
+from src.keyboard import *
+from src.floodControl import floodControl
+
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+
+#: Text handler
+@bot.message_handler(content_types=['text'])
+def message(message):
+    userLanguage  = dbSql.getSetting(message.from_user.id, 'language')
+
+    if floodControl(message, userLanguage):
+        #! Start message handler
+        if message.text == '/start':
+            bot.send_message(message.chat.id, language['greet'][userLanguage], reply_markup=startKeyboard(userLanguage))
+
+        #! Link message handler
+        else:
+            url = message.text.split('?')[0].lower()
+            url = 'https' + url if not url.startswith('http') else url
+            
+            #! Check if the URL is already in the database
+            videoId = dbSql.getVideo(url=url)
+            setUrlVideoId = False
+            setRcVideoId = False
+            
+            if not videoId:
+                setUrlVideoId = True
+                video = getVideo(message.text)
+
+                if video['success']:
+                    videoLink = video['link']
+
+                    #! Getting the rc parameter from the link
+                    rc = parse_qs(urlparse(videoLink).query)['rc'][0]
+
+                    #! Check if the rc is already in the database
+                    videoId = dbSql.getVideo(rc=rc)
+                    if not videoId:
+                        videoId = videoLink
+                        setRcVideoId = True
+
+            #! If video download is successful
+            if videoId:
+                bot.send_chat_action(message.chat.id, 'upload_video')
+                sent = bot.send_video(message.chat.id, videoId if setRcVideoId else videoId['videoId'], reply_markup=resultKeyboard(userLanguage, url))
+                bot.delete_message(message.chat.id, message.id)
+
+                if setRcVideoId:
+                    dbSql.setVideo(rc=rc, url=url, videoId=sent.video.file_id, duration=sent.video.duration, description=video['description'])
+
+                elif setUrlVideoId:
+                    dbSql.setVideo(url=url, rc=rc, setRc=False)
+            
+            #! Error
+            else:
+                bot.send_message(message.chat.id, language[video['error']][userLanguage], reply_markup=socialKeyboard(userLanguage) if video['error'] in ['exception', 'unknownError'] else None)
